8b8bd4129cb4f488bd78c5c746d3933886562075
amir-398 <amir.398@hotmail.fr>
2024-05-30 01:15:55 +0200

    chore: Mettre à jour le composant Header avec le titre "Paris" et une icône de notification

--- src/screens/home/components/Header.tsx
+++ src/screens/home/components/Header.tsx
@@ -1,13 +1,22 @@
+import notifIcon from "@/assets/icons/notif.png";
 import logo from "@/assets/logo/logo.png";
-import { Icon } from "@rneui/themed";
+import FONTS from "@/constants/FONTS";
 import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 export default function Header() {
   return (
     <View style={styles.container}>
-      <Icon name="filter-circle" type="ionicon" size={30} />
-      <Image style={styles.logo} source={logo} />
-      <Icon name="notifications" type="ionicon" size={30} />
+      <View style={styles.left}>
+        <Text style={styles.title}>Paris</Text>
+      </View>
+      <View style={styles.center}>
+        <Image style={styles.logo} source={logo} />
+      </View>
+      <View style={styles.right}>
+        <Pressable>
+          <Image source={notifIcon} style={styles.notifIcon} />
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -21,8 +30,27 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: "#f1f1f1",
   },
+  left: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: FONTS.poppinsBold,
+  },
+  center: {
+    flex: 1,
+    alignItems: "center",
+  },
   logo: {
     width: 130,
     height: 30,
+  },
+  right: {
+    flex: 1,
+    alignItems: "flex-end",
+  },
+  notifIcon: {
+    width: 25,
+    height: 25,
   },
 });
